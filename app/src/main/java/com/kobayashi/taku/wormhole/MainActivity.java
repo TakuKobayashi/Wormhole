27e0279f12bcaa546a13e81ae31dd77cc2cb8c66
TakuKobayshi <>
2017-08-22 12:25:07 +0900

    秘伝のタレとか色々と持ってきてWebRTCが動くところまで持ってきた

--- app/src/main/java/com/kobayashi/taku/wormhole/MainActivity.java
+++ app/src/main/java/com/kobayashi/taku/wormhole/MainActivity.java
@@ -1,13 +1,39 @@
 package com.kobayashi.taku.wormhole;
 
+import android.Manifest;
+import android.app.Application;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_CAMERA_PERMISSION = 1;
+    private WebRTC webRTC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Util.requestPermissions(this, REQUEST_CODE_CAMERA_PERMISSION);
+        Log.d("wormhole", String.valueOf(Util.checkAllPermissionsAccept(this)));
+        startCapture();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        if (requestCode != REQUEST_CODE_CAMERA_PERMISSION)
+            return;
+        if(Util.checkAllPermissionsAccept(this)){
+            startCapture();
+        }
+    }
+
+    private void startCapture(){
+        Log.d("wormhole", "startCapture");
+        webRTC = new WebRTC(this);
+        webRTC.startCapture();
     }
 }
---
+++ app/src/main/java/com/kobayashi/taku/wormhole/WebRTC.java
@@ -0,0 +1,107 @@
+package com.kobayashi.taku.wormhole;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+import org.webrtc.Camera2Enumerator;
+import org.webrtc.CameraEnumerator;
+import org.webrtc.EglBase;
+import org.webrtc.MediaStream;
+import org.webrtc.PeerConnectionFactory;
+import org.webrtc.RendererCommon;
+import org.webrtc.SurfaceViewRenderer;
+import org.webrtc.VideoCapturer;
+import org.webrtc.VideoRenderer;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
+
+public class WebRTC {
+    private Activity activity;
+    private PeerConnectionFactory factory;
+    private VideoCapturer videoCapturer;
+    private EglBase.Context renderEGLContext;
+
+    public WebRTC(Activity activity){
+        this.activity = activity;
+
+        // rendereContext
+        EglBase eglBase = EglBase.create();
+        renderEGLContext = eglBase.getEglBaseContext();
+
+        // initialize Factory
+        PeerConnectionFactory.initializeAndroidGlobals(activity.getApplicationContext(), true);
+        PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
+        factory = new PeerConnectionFactory(options);
+        factory.setVideoHwAccelerationOptions(renderEGLContext, renderEGLContext);
+
+        // setupLocalStream
+        setupLocalStream();
+    }
+
+    // interface -----------------
+
+    public void startCapture(){
+        _startCapture();
+    }
+
+    // implements -------------
+
+    private void setupLocalStream() {
+
+        SurfaceViewRenderer localRenderer = setupRenderer();
+
+        MediaStream localStream = factory.createLocalMediaStream("android_local_stream");
+        videoCapturer = createCameraCapturer(new Camera2Enumerator(activity));
+        VideoSource localVideoSource = factory.createVideoSource(videoCapturer);
+
+        VideoTrack localVideoTrack = factory.createVideoTrack("android_local_videotrack", localVideoSource);
+        localStream.addTrack(localVideoTrack);
+
+        VideoRenderer videoRender = new VideoRenderer(localRenderer);
+        localVideoTrack.addRenderer(videoRender);
+    }
+
+
+    private void _startCapture(){
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) activity.getApplication().getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getRealMetrics(displayMetrics);
+        int videoWidth = displayMetrics.widthPixels;
+        int videoHeight = displayMetrics.heightPixels;
+
+        videoCapturer.startCapture(videoWidth, videoHeight, 30);
+    }
+
+    private SurfaceViewRenderer setupRenderer(){
+        SurfaceViewRenderer localRenderer = (SurfaceViewRenderer) activity.findViewById(R.id.local_render_view);
+        localRenderer.init(renderEGLContext, null);
+        localRenderer.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FILL);
+        localRenderer.setZOrderMediaOverlay(true);
+        localRenderer.setEnableHardwareScaler(true);
+
+        return localRenderer;
+    }
+
+    private VideoCapturer createCameraCapturer(CameraEnumerator enumerator) {
+        return createBackCameraCapturer(enumerator);
+    }
+
+    private VideoCapturer createBackCameraCapturer(CameraEnumerator enumerator) {
+        final String[] deviceNames = enumerator.getDeviceNames();
+
+        for (String deviceName : deviceNames) {
+            if (!enumerator.isFrontFacing(deviceName)) {
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName, null);
+
+                if (videoCapturer != null) {
+                    return videoCapturer;
+                }
+            }
+        }
+
+        return null;
+    }
+}
