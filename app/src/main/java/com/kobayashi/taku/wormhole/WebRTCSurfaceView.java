2dc5e5241e74b76fd19b3d55d8456353fbda3379
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-08-23 04:00:38 +0900

    大シャバな修正だけど一旦コミット

--- app/src/main/java/com/kobayashi/taku/wormhole/WebRTCSurfaceView.java
+++ app/src/main/java/com/kobayashi/taku/wormhole/WebRTCSurfaceView.java
@@ -8,21 +8,39 @@ import org.webrtc.RendererCommon;
 import org.webrtc.SurfaceViewRenderer;
 
 public class WebRTCSurfaceView extends SurfaceViewRenderer {
+    private EglBase.Context mRenderEGLContext;
+
     public WebRTCSurfaceView(Context context) {
         super(context);
+        Setup();
     }
 
     public WebRTCSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        Setup();
     }
 
     public void Setup(){
         // rendereContext
         EglBase eglBase = EglBase.create();
-        renderEGLContext = eglBase.getEglBaseContext();
-        this.init(renderEGLContext, null);
+        mRenderEGLContext = eglBase.getEglBaseContext();
+        this.init(mRenderEGLContext, new RendererCommon.RendererEvents() {
+            @Override
+            public void onFirstFrameRendered() {
+
+            }
+
+            @Override
+            public void onFrameResolutionChanged(int i, int i1, int i2) {
+
+            }
+        });
         this.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FILL);
         this.setZOrderMediaOverlay(true);
         this.setEnableHardwareScaler(true);
+    }
+
+    public EglBase.Context getRenderEGLContext(){
+        return mRenderEGLContext;
     }
 }
