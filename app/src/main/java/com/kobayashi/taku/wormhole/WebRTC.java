bb3ade3405409ff82adb011ce619bdd32867beef
TakuKobayshi <>
2017-08-30 17:37:06 +0900

    connectたたき台

--- app/src/main/java/com/kobayashi/taku/wormhole/WebRTC.java
+++ app/src/main/java/com/kobayashi/taku/wormhole/WebRTC.java
@@ -8,18 +8,25 @@ import android.view.WindowManager;
 
 import org.webrtc.Camera2Enumerator;
 import org.webrtc.CameraEnumerator;
+import org.webrtc.DataChannel;
 import org.webrtc.EglBase;
+import org.webrtc.IceCandidate;
+import org.webrtc.MediaConstraints;
 import org.webrtc.MediaStream;
+import org.webrtc.PeerConnection;
 import org.webrtc.PeerConnectionFactory;
 import org.webrtc.RendererCommon;
+import org.webrtc.RtpReceiver;
 import org.webrtc.SurfaceViewRenderer;
 import org.webrtc.VideoCapturer;
 import org.webrtc.VideoRenderer;
 import org.webrtc.VideoSource;
 import org.webrtc.VideoTrack;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.UUID;
 
 public class WebRTC {
@@ -39,6 +46,75 @@ public class WebRTC {
         factory = new PeerConnectionFactory(options);
 
         camera = new WebRTCCamera(activity);
+    }
+
+    public void connect(){
+        // initialize Factory
+        PeerConnectionFactory.initializeAndroidGlobals(activity.getApplicationContext(), true);
+        PeerConnectionFactory.Options options = new PeerConnectionFactory.Options();
+        factory = new PeerConnectionFactory(options);
+
+        // create PeerConnection
+        List<PeerConnection.IceServer> iceServers = Arrays.asList(new PeerConnection.IceServer("stun:stun.l.google.com:19302"));
+        // 何かセットする必要が多分ある
+        MediaConstraints constraints = new MediaConstraints();
+        factory.createPeerConnection(iceServers, constraints, new PeerConnection.Observer() {
+            @Override
+            public void onSignalingChange(PeerConnection.SignalingState signalingState) {
+
+            }
+
+            @Override
+            public void onIceConnectionChange(PeerConnection.IceConnectionState iceConnectionState) {
+
+            }
+
+            @Override
+            public void onIceConnectionReceivingChange(boolean b) {
+
+            }
+
+            @Override
+            public void onIceGatheringChange(PeerConnection.IceGatheringState iceGatheringState) {
+
+            }
+
+            @Override
+            public void onIceCandidate(IceCandidate iceCandidate) {
+
+            }
+
+            @Override
+            public void onIceCandidatesRemoved(IceCandidate[] iceCandidates) {
+
+            }
+
+            @Override
+            public void onAddStream(MediaStream mediaStream) {
+
+            }
+
+            @Override
+            public void onRemoveStream(MediaStream mediaStream) {
+
+            }
+
+            @Override
+            public void onDataChannel(DataChannel dataChannel) {
+
+            }
+
+            @Override
+            public void onRenegotiationNeeded() {
+
+            }
+
+            @Override
+            public void onAddTrack(RtpReceiver rtpReceiver, MediaStream[] mediaStreams) {
+
+            }
+        });
+        //peerConnection = factory.createPeerConnection(iceServers, WebRTCUtil.peerConnectionConstraints(), this);
     }
 
     public String addLocalView(WebRTCSurfaceView view){
